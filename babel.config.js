e88575955f7e178d5d9a7f37f01bf5ee4f5c6ff7
miaoda <miaoda@baidu.com>
2026-06-28 14:23:16 -0400

    animaciones

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   presets: [['miaoda-expo-devkit/babel-preset', { excludePaths: ['src/components/ui'] }]],
+  plugins: [
+    'react-native-reanimated/plugin', // 👈 Necesario para las animaciones
+  ],
 };
